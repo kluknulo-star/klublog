770339ee4d46c3d27794af1cc609d8d56dfe62ab
kirill-kilyakov <kluknulo@mail.ru>
2023-03-15 17:48:31 +0300

    Seed Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $admin = User::create([
+            'name' => 'Админ всея Руси',
+            'email' => 'admin@kluknulo.ru',
+            'password' => Hash::make('admin@kluknulo.ru'),
+        ]);
     }
 }
